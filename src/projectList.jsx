b5942256b9009288c37d59d655a5e38e35a49460
SeanLukeBalbin <sean.balbin@bigtimeempire.com>
2025-09-23 11:26:16 +0800

    changes agian

--- src/projectList.jsx
+++ src/projectList.jsx
@@ -1,11 +1,11 @@
-import store201 from "./assets/store201.png"
+import store201 from "./assets/store201.png";
 import tailWindCSS from "./picture/tailwind.png";
 import cSharp from "./picture/csharp.png";
 import mySQL from "./picture/sql.png";
 import tago from "./assets/tago.png";
-import blender from "./picture/blenderr.png"
+import blender from "./picture/blenderr.png";
 import unity from "./picture/unityy.png";
-import react from './picture/react.png'
+import react from "./picture/react.png";
 
 export const projectList = [
   {
@@ -13,25 +13,13 @@ export const projectList = [
     description:
       "The Restaurant 201 Information System aims to centralize store information for marketing and operations departments, allowing easy access to store data without requiring IT intervention",
     picture: store201,
-    logos: [
-        tailWindCSS, cSharp, mySQL, react
-    ],
+    logos: [tailWindCSS, cSharp, mySQL, react],
   },
   {
-    name:"TAGO",
-    description:"TAGO is a horror game that blends survival, strategy, role-playing, and crafting elements, offering players a distinctive experience centered around carefully selected folklore creatures from the Philippines.",
+    name: "TAGO",
+    description:
+      "TAGO is a horror game that blends survival, strategy, role-playing, and crafting elements, offering players a distinctive experience centered around carefully selected folklore creatures from the Philippines.",
     picture: tago,
-    logos:[cSharp, blender,unity 
-
-    ]
-
-
+    logos: [cSharp, blender, unity],
   },
-  
-  
-  
-
 ];
-
-
-export default projectList;
